bc09c9fc33e6535e51ee1a1201124eb0952fc85e
jsoderkvist <jean.soderkvist@gmail.com>
2013-07-07 20:51:20 -0500

    Add more menuitem specs

--- spec/menuitem_spec.rb
+++ spec/menuitem_spec.rb
@@ -5,6 +5,18 @@ describe MenuItem do
 		@item = MenuItem.new("name", "2.25")
 	end
 
+	describe "#name" do
+		it "returns item name" do
+			@item.name.should eql "name"
+		end
+	end
+
+	describe "#price" do
+		it "returns item price as BigDecimal" do
+			@item.price.should eql BigDecimal.new("2.25")
+		end
+	end	
+
 	describe "#to_s" do
 		it "formats name and price with symbol" do
 			@item.to_s.should eql "name ($2.25)"
